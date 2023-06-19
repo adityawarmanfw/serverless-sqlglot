f54976921efd55787aa37f2292b1ddfa4b25d763
Fidocia Wima Adityawarman <adityawarmanfw@gmail.com>
2023-06-19 19:57:36 +0700

    update lineage function, add columns lineage

--- api/index.py
+++ api/index.py
@@ -7,20 +7,37 @@ from sqlglot import exp
 def table_ref(table):
     return "{0}.{1}".format(table.text("db"), table.text("this")) if table.text("db") else table.text("this")
 
+def get_columns_lineage(column):
+    if "table" in column.args:
+        parent_table = column.args["table"].text("this")
+    else:
+        parent_table = "LITERAL"
+    parent_column = column.text("this")
+    lineage = {"table": parent_table, "col": parent_column}
+    return lineage
 
-def find_columns(select_statement):
+def get_columns(select_statement):
     column_names = []
     for expression in select_statement:
         if isinstance(expression, exp.Alias):
-            column_names.append(
-                {"name": expression.text("alias"), "sql": expression.sql()})
-        elif isinstance(expression, exp.Column):
-            column_names.append(
-                {"name": expression.text("this"), "sql": expression.sql()})
-    return column_names if column_names else [{"name": 'all', "sql": '*'}]
+            column_parents = []
+
+            for column in expression.find_all(exp.Column):
+                column_parents.append(get_columns_lineage(column))
+
+            for literal in expression.find_all(exp.Literal):              
+                column_parents.append(get_columns_lineage(literal))
+            
+            column_names.append({
+                "col": expression.text("alias"), 
+                "parents": list(column_parents), 
+                "sql": expression.sql()
+                })
+                
+    return column_names if column_names else [{"col": "STAR", "parents": [], "sql": "*"}]
 
 
-def find_tables(table_statement):
+def get_tables(table_statement):
     table_names = []
     if isinstance(table_statement, exp.From):
         for tables in table_statement.find_all(exp.Table):
@@ -39,30 +56,30 @@ def get_selects(select_stmt, name, kind):
     raw_query = select_stmt.sql()
     for select in select_stmt.find_all(exp.Select):
         sql = select.sql()
-        cols = find_columns(select.args['expressions'])
-        if 'from' in select.args:
-            tables.extend(find_tables(select.args['from']))
-        if 'joins' in select.args:
-            tables.extend(find_tables(select.args['joins']))
+        cols = get_columns(select.args["expressions"])
+        if "from" in select.args:
+            tables.extend(get_tables(select.args["from"]))
+        if "joins" in select.args:
+            tables.extend(get_tables(select.args["joins"]))
 
     selects = {
-        'id': name,
-        'cols': cols,
-        'parents': list(set(tables)),
-        'sql': raw_query,
-        'kind': kind
+        "table": name,
+        "kind": kind,
+        "cols": cols,
+        "table_parents": list(set(tables)),
+        "table_sql": raw_query,
     }
 
     return selects
 
 
-def get_structure(sql):
-    ast = sqlglot.parse_one(sql)
+def get_lineage(sql):
+    ast = qualify_columns(sqlglot.parse_one(sql), schema=None)
 
     final_model = []
 
-    if 'with' in ast.args:
-        for cte in ast.find(exp.With).args['expressions']:
+    if "with" in ast.args:
+        for cte in ast.find(exp.With).args["expressions"]:
             name = cte.find(exp.TableAlias).text("this")
             final_model.append(get_selects(cte, name, "CTE"))
 
@@ -75,24 +92,24 @@ def get_structure(sql):
 
     base_tables = []
     for entry in final_model:
-        parents = entry.get('parents', [])
+        parents = entry.get("parents", [])
 
         for parent in parents:
-            if not any(item['id'] == parent for item in final_model):
+            if not any(item['table'] == parent for item in final_model):
                 if parent not in base_tables:
                     base_tables.append({
-                        'id': parent,
-                        'cols': [{'name': 'all', 'sql': '*'}],
-                        'parents': [],
-                        'sql': f"select * from {parent}",
-                        'kind': 'SOURCE'
+                        "table": parent,
+                        "kind": "SOURCE",
+                        "cols": [{"name": "STAR", "parents": [], "sql": "*"}],
+                        "table_parents": [],
+                        "table_sql": f"SELECT * FROM {parent}",
                     })
 
     return final_model + base_tables
 
 
 app = Flask(__name__)
-
+app.config["JSONIFY_PRETTYPRINT_REGULAR"] = True
 
 @app.route('/ast')
 def parse_to_json():
@@ -112,7 +129,7 @@ def parse_to_json():
     except Exception as e:
         return jsonify(error=str(e)), 400
 
-@app.route('/cte-tables')
+@app.route('/lineage')
 def lineage_to_json():
     # Retrieve the 'sql' query parameter from the URL
     sql = request.args.to_dict().get('sql')
@@ -121,7 +138,7 @@ def lineage_to_json():
         return jsonify(error='SQL query is missing'), 400
 
     try:
-        model = get_structure(sql)
+        model = get_lineage(sql)
         return jsonify(model)
     except Exception as e:
         return jsonify(error=str(e)), 400
